2ee85cbf95a3a1530461d296952da19708337fe5
YuliiaMakhovska <y.makhovska@gmail.com>
2022-10-23 23:49:15 +0300

    update files

---
+++ src/js/fetchPictures.js
@@ -0,0 +1,12 @@
+// import axios from 'axios';
+// export { fetchPictures };
+// const axios = require('axios').default;
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const KEY = '30705371-14a7eb31a425b5bed29cbaaa0';
+
+// async function fetchPictures(name, page, perPage) {
+//   const response = await axios.get(
+//     `?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+//   );
+//   return response;
+// }
